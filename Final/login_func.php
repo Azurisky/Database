4e4bbbdd592742404826da34e0daff32dad46906
Azur <andrew0704us@gmail.com>
2014-04-14 18:45:19 +0800

    optimized project 2

--- Final/login_func.php
+++ Final/login_func.php
@@ -46,8 +46,9 @@
 	else
 		$_SESSION['Identity'] = false;*/
 	
-	if(($result->account==$_Account && $result->password == $_Hash && $result->is_admin == 1) || ($_SESSION['Identity'] == 2))
+	if($_SESSION['Identity'] == 2)
 		{	
+			
 			$_SESSION['Account'] = $_Account;
 			$_SESSION['ID'] = $result->ID;
 			$_SESSION['Password'] = $result->password;
@@ -55,6 +56,7 @@
 			$sql = "SELECT * FROM `Flight` ORDER BY  `".$_Type."` ".$_Info;
 			$sth=$db->prepare($sql);
 			$sth->execute();
+			//echo "<p>Admin_by_session</p>";
 			echo "<table border=\"1\">";
 			echo "<form action = \"newflight.php\" >
 						<button type = \"submit\">Create New Flight</button>
@@ -175,8 +177,9 @@
 			
 	
 		}
-	else if(($result->account==$_Account && $result->password == $_Hash && $result->is_admin == 0) || ($_SESSION['Identity'] == 1))
-		{
+	else if($_SESSION['Identity'] == 1) 
+	{	
+			
 			$_SESSION['Identity'] = 1;
 			$_SESSION['Account'] = $_Account;
 			$_SESSION['ID'] = $result->ID;
@@ -184,7 +187,247 @@
 			$sql = "SELECT * FROM `Flight` ORDER BY  `".$_Type."` ".$_Info;
 			$sth=$db->prepare($sql);
 			$sth->execute();
+			//echo "<p>User_by_session</p>";
+			echo "<table border=\"1\">";
+			echo "<form action = \"comparisonlist.php\" >
+						<button type = \"submit\">ComparisonList</button>
+				  </form>
+				  </table>";
 			
+			echo "<table border=\"1\">";
+			echo "<form action = \"seaflight_func.php\" method = \"POST\">
+					Please enter the information you want to search:
+				  <select name = \"Type\">
+					<option value = \"Flight_number\">Flight number</option>
+					<option value = \"Departure\">Departure</option>
+					<option value = \"Destination\">Destination</option>
+				  </select>"."
+					<input type=\"text\" name=\"Info\">
+					<button type=\"submit\">submit</button>
+				</form></table>";
+				
+			echo "<table border=\"1\">";
+			echo "<form action = \"login_func.php\" method = \"POST\">
+				 Order by:
+				  <select name = \"Type\">
+					<option value = \"ID\">ID</option>
+					<option value = \"Flight_number\">Flight number</option>
+					<option value = \"Departure\">Departure</option>
+					<option value = \"Destination\">Destination</option>
+					<option value = \"Departure_time\">Departure_time</option>
+					<option value = \"Arrival_time\">Arrival_time</option>
+					<option value = \"Price\">Price</option>
+				  </select>"."
+					Please choose a way to order:
+					<input type=\"radio\" name=\"Info\" value=\"ASC\" checked>Ascending order
+					<input type=\"radio\" name=\"Info\" value=\"DESC \">Descending order
+					<button type=\"submit\">submit</button>
+				</form></table>";
+			
+			echo "<table border=\"1\">";
+			echo "<tr>
+						<td>ID</td>
+						<td>Flight_number</td>
+						<td>Departure</td>
+						<td>Destination</td>
+						<td>Departure_time</td>
+						<td>Arrival_time</td>
+						<td>Price</td>
+						<td>Favorite</td>
+						<td></td>
+				  </tr>";
+			while($result=$sth->fetchObject())
+				 
+				 {
+					echo 
+					"<tr>
+							<th>".$result->ID ."</th>
+							<th>".$result->Flight_number."</th>
+							<th>".$result->Departure."</th>
+							<th>".$result->Destination."</th>
+							<th>".$result->Departure_time."</th>
+							<th>".$result->Arrival_time."</th>
+							<th>".$result->Price."</th>";
+					$in_list=0;
+				
+					$_db = new PDO($dsn,$db_user,$db_password);
+					$_sql = "SELECT * FROM `".$_Account."`";
+					$_sth=$_db->prepare($_sql);
+					$_sth->execute(array());
+					
+					while($_result=$_sth->fetchObject())
+					{
+						if($_result->ID==$result->ID)$in_list=1;
+					}
+					if($in_list==1)
+					{
+
+						echo "<th>O</th>
+							  <th><form action = \"delfromlist.php\" method=\"POST\" >
+									 <button name = \"Flight_ID\" value =\"".$result->ID."\" type=\"submit\">Delete</button>
+								  </form></th>";
+						 
+					}
+					if($in_list==0)
+					{
+						echo"<th>X</th>
+							 <th><form action = \"addtolist.php\" method=\"POST\" >
+									<button name = \"Flight_ID\" value =\"".$result->ID."\">Add</button>
+							     </form></th>";
+					}
+					echo "</tr>";
+					
+				 }
+			echo "</table>";
+				
+			echo  "<p><form action = \"login.php\" >
+						<button type = \"submit\">Log out</button>
+				  </form></p>";
+			
+		
+		}
+	else if($result->account==$_Account && $result->password == $_Hash && $result->is_admin == 1)
+	{	
+			
+			$_SESSION['Account'] = $_Account;
+			$_SESSION['ID'] = $result->ID;
+			$_SESSION['Password'] = $result->password;
+			$_SESSION['Identity'] = 2;
+			$sql = "SELECT * FROM `Flight` ORDER BY  `".$_Type."` ".$_Info;
+			$sth=$db->prepare($sql);
+			$sth->execute();
+			//echo "<p>Admin_by_log</p>";
+			echo "<table border=\"1\">";
+			echo "<form action = \"newflight.php\" >
+						<button type = \"submit\">Create New Flight</button>
+				  </form>".
+				  "<form action = \"modflight.php\" >
+						<button type = \"submit\">Modify Flight</button>
+				  </form>".
+				  "<form action = \"delflight.php\" >
+						<button type = \"submit\">Delete Flight</button>
+				  </form>
+				  </table>";
+				  
+			echo "<table border=\"1\">";
+			echo "<form action = \"seaflight_func.php\" method = \"POST\">
+					Please enter the information you want to search:
+				  <select name = \"Type\">
+					<option value = \"Flight_number\">Flight number</option>
+					<option value = \"Departure\">Departure</option>
+					<option value = \"Destination\">Destination</option>
+				  </select>"."
+					<input type=\"text\" name=\"Info\">
+					<button type=\"submit\">submit</button>
+				</form></table>";
+				
+			echo "<table border=\"1\">";
+			echo "<form action = \"login_func.php\" method = \"POST\">
+				 Order by:
+				  <select name = \"Type\">
+					<option value = \"ID\">ID</option>
+					<option value = \"Flight_number\">Flight number</option>
+					<option value = \"Departure\">Departure</option>
+					<option value = \"Destination\">Destination</option>
+					<option value = \"Departure_time\">Departure_time</option>
+					<option value = \"Arrival_time\">Arrival_time</option>
+					<option value = \"Price\">Price</option>
+				  </select>"."
+					Please choose a way to order:
+					<input type=\"radio\" name=\"Info\" value=\"ASC\" checked>Ascending order
+					<input type=\"radio\" name=\"Info\" value=\"DESC \">Descending order
+					<button type=\"submit\">submit</button>
+				</form></table>";
+				 
+				 
+				  
+			
+			echo "<table border=\"1\">";
+			echo "<tr>
+						<td>ID</td>
+						<td>Flight_number</td>
+						<td>Departure</td>
+						<td>Destination</td>
+						<td>Departure_time</td>
+						<td>Arrival_time</td>
+						<td>Price</td>
+						<td>Favorite</td>
+						<td></td>
+				  </tr>";
+			while($result=$sth->fetchObject())
+				 
+				 {
+					echo 
+					"<tr>
+							<th>".$result->ID ."</th>
+							<th>".$result->Flight_number."</th>
+							<th>".$result->Departure."</th>
+							<th>".$result->Destination."</th>
+							<th>".$result->Departure_time."</th>
+							<th>".$result->Arrival_time."</th>
+							<th>".$result->Price."</th>";
+					$in_list=0;
+				
+					$_db = new PDO($dsn,$db_user,$db_password);
+					$_sql = "SELECT * FROM `".$_Account."`";
+					$_sth=$_db->prepare($_sql);
+					$_sth->execute(array());
+					
+					while($_result=$_sth->fetchObject())
+					{
+						if($_result->ID==$result->ID)$in_list=1;
+					}
+					if($in_list==1)
+					{
+
+						echo "<th>O</th>
+							  <th><form action = \"delfromlist.php\" method=\"POST\" >
+									 <button name = \"Flight_ID\" value =\"".$result->ID."\" type=\"submit\">Delete</button>
+								  </form></th>";
+						 
+					}
+					if($in_list==0)
+					{
+						echo"<th>X</th>
+							 <th><form action = \"addtolist.php\" method=\"POST\" >
+									<button name = \"Flight_ID\" value =\"".$result->ID."\">Add</button>
+							     </form></th>";
+					}
+					echo "</tr>";
+					
+				 }
+				
+				
+			
+			
+			echo "</table>";
+			echo "<table border=\"1\">";
+			echo  "<form action = \"user.php\" >
+						<button type = \"submit\">userlist</button>
+				  </form>";
+			echo  "<form action = \"airport.php\" >
+						<button type = \"submit\">airportlist</button>
+				  </form>
+				  <form action = \"comparisonlist.php\" >
+						<button type = \"submit\">ComparisonList</button>
+				  </form></table>";
+			echo "<p><form action = \"login.php\" >
+						<button type = \"submit\">Log out</button>
+				  </form></p>";
+			
+	
+		}
+	else if(($result->account==$_Account && $result->password == $_Hash && $result->is_admin == 0))
+		{
+			
+			$_SESSION['Identity'] = 1;
+			$_SESSION['Account'] = $_Account;
+			$_SESSION['ID'] = $result->ID;
+			$_SESSION['Password'] = $result->password;
+			$sql = "SELECT * FROM `Flight` ORDER BY  `".$_Type."` ".$_Info;
+			$sth=$db->prepare($sql);
+			$sth->execute();
+			//echo "<p>User_by_log</p>";
 			echo "<table border=\"1\">";
 			echo "<form action = \"comparisonlist.php\" >
 						<button type = \"submit\">ComparisonList</button>
